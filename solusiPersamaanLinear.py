b741e7df8eddba135976fe1cec51414742cceda7
Shalahuddin Yoga Wardana Abdullah <70340926+WardanaYoga@users.noreply.github.com>
2025-08-16 21:51:55 +0700

    Create solusiPersamaanLinear.py

---
+++ solusiPersamaanLinear.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+A = np.array([(2,3), (1,2)])
+Y = np.array ([23, 14])
+
+print(A)
+print(Y)
+
+# menyelesaikan persamaan linear
+A_inv = np.linalg.inv(A)
+X = np.dot(A_inv, Y)
+
+print(X)
+
+# cara lain
+X2 = np.linalg.solve(A,Y)
+print(X2)
